045d35902f1978b44759614fc59eb5ab3a29f7e2
Aditya K <adityavkk@gmail.com>
2016-11-16 15:11:19 -0500

    finish 1.10

--- chapter-1/exercises.scm
+++ chapter-1/exercises.scm
@@ -198,3 +198,7 @@
 ; 2^(2^(2^(2)))
 ; 2^16 = 6536
 
+(define (f n) (A 0 n)) ; (A 0 n) -> (* 2 n)
+(define (g n) (A 1 n)) ; (A 1 n) -> (pow 2 n)
+(define (h n) (A 2 n)) ; (A 2 n) -> 2^2^2....^2 n times
+
