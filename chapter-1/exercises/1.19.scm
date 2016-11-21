7eca3ce24fde420e98e5b58b3c3fd853e9a6c9ff
Aditya K <adityavkk@gmail.com>
2016-11-21 12:27:38 -0500

    complete 1.19, logrithimic fibonacci

--- chapter-1/exercises/1.19.scm
+++ chapter-1/exercises/1.19.scm
@@ -10,4 +10,45 @@
 ; of p and q. This gives us an explicit way of squaring this transformation and thus
 ; we can compute T^n using succesive squaring.
 
+;;;;;;;;;; Solution ;;;;;;;;;;
+; We know that Tp'q' is of the same form as Tpq:
+
+; ∴ Tp'q' =>
+;            a <- bq' + aq' + ap'
+;            b <- bp' + aq'
+
+; We also know that Tp'q' is T^2
+; ∴ Tp'q' =>
+;            a <- (bp + aq) q + (bq + aq + ap) q + (bq + aq + ap) p
+;                 = b (q^2 + 2pq) + a (p^2 + q^2) + a (q^2 + 2pq)
+;            b <- (bp + aq) p + (bq + aq + ap) q
+;                 = b (p^2 + q^2) + a (2pq + q^2)
+
+; ∴  p' = (p^2 + q^2)
+;    q' = (2pq + q^2)
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (square n)
+  (* n n))
+
+(define (fib-iter a b p q count)
+  (define (p-prime p q)
+    (+ (square p) (square q)))
+  (define (q-prime p q)
+    (+ (* 2 p q) (square q)))
+  (cond ((= count 0) b)
+        ((even? count)
+          (fib-iter a
+                    b
+                    (p-prime p q)
+                    (q-prime p q)
+                    (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+
 
