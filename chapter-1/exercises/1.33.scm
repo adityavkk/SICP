a6384cbeb3f956157f4fdb77487eb4a958487821
Aditya K <adityavkk@gmail.com>
2016-11-23 17:59:48 -0500

    complete 1.33

---
+++ chapter-1/exercises/1.33.scm
@@ -0,0 +1,36 @@
+; 1.33
+(define (filtered-accumulate filter-fn comb null-val term a next b)
+  (if (> a b)
+      null-val
+      (if (filter-fn a)
+          (comb (term a)
+                (filtered-accumulate filter-fn comb null-val term (next a) next b))
+          (filtered-accumulate filter-fn comb null-val term (next a) next b))))
+
+(define (sum-of-prime-squares a b)
+  (filtered-accumulate prime? + 0 square a inc b)) ; <- (sum-of-prime-squares 2 5) = 38
+
+(define (inc x) (+ 1 x))
+
+(define (prime? n)
+  (fermat-test n 100))
+
+(define (fermat-test n times)
+  (cond ((= times 0) #t)
+        ((fermat n) (fermat-test n (- times 1)))
+        (else #f)))
+
+(define (fermat n)
+  (define (try a)
+    (= (expmod a n n) a))
+  (try (+ 1 (random (- n 1)))))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+          (remainder (square (expmod base (/ exp 2) m)) m))
+        (else
+          (remainder (* base (expmod base (- exp 1) m)) m))))
+
+(define (square n)
+  (* n n))
