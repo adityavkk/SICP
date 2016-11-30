fa2f5aa144782ed9ff7229ab25d7d4a8ce1a82da
Aditya K <adityavkk@gmail.com>
2016-11-30 17:33:37 -0500

    complete 1.37 b

--- chapter-1/exercises/1.37.scm
+++ chapter-1/exercises/1.37.scm
@@ -4,6 +4,9 @@
 (define (cont-frac n d k)
   (cont-frac-rec n d k 1))
 
+(define (count-frac-prime n d k)
+  (cont-frac-iter n d k k 0))
+
 (define (cont-frac-rec n d k i)
   (let ((ni (n i))
         (di (d i)))
@@ -14,10 +17,22 @@
                                    k
                                    (+ i 1)))))))
 
+(define (cont-frac-iter n d k i res)
+  (let ((ni (n i))
+        (di (d i)))
+    (cond ((= k i) (cont-frac-iter n d k (- i 1) (/ ni di)))
+          ((= i 0) res)
+          (else (cont-frac-iter n d k (- i 1) (/ ni (+ di res)))))))
+
 (define (calc-phi k)
   (cont-frac (lambda (i) 1.0)
              (lambda (i) 1.0)
              k))
+
+(define (calc-phi-prime k)
+  (count-frac-prime (lambda (i) 1.0)
+                    (lambda (i) 1.0)
+                    k))
 
 (define (close-enough? guess val)
   (< (abs (- guess val)) 0.0001))
@@ -28,5 +43,7 @@
       i
       (inverse-phi-steps (+ i 1))))
 
-(display (inverse-phi-steps 1.0))
+(display (inverse-phi-steps 1.0)) ; 10 steps
+(newline)
+(display (calc-phi-prime 11.0))
 (newline)
